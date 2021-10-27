31d2b4f7c105ef8097c7c758554ea5efc7320ccc
Devin Kearns <devin.kearns@outlook.com>
2021-10-27 13:54:35 -0400

    initial commit

---
+++ react/first_jsx/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="container">
+        <h1>Hello Dojo</h1>
+        <h4>Things I need to do:</h4>
+        <ul>
+          <li>Go to the grocery store</li>
+          <li>Vacuum my room</li>
+          <li>Respond to hard mail</li>
+          <li>Complete career services assignment</li>
+          <li>Respond to mail</li>
+          <li>Work through this morning's algo + quick sort</li>
+          <li>Read react resources</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
