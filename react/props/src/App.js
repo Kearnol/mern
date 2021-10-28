e642add0f268851c8c9980a7506baa683a53ea22
Devin Kearns <devin.kearns@outlook.com>
2021-10-28 18:11:25 -0400

    added birthday button

--- react/props/src/App.js
+++ react/props/src/App.js
@@ -6,10 +6,10 @@ import PersonCard from './components/PersonCard';
 
 function App() {
   var person = [
-    {"firstName": "Jane", "lastName": "Doe", "age": "45", "hairColor": "Black"},
-    {"firstName": "John", "lastName": "Smith", "age": "88", "hairColor": "Brown"},
-    {"firstName": "Millard", "lastName": "Fillmore", "age": "50", "hairColor": "Brown"},
-    {"firstName": "Maria", "lastName": "Smith", "age": "62", "hairColor": "Brown"}
+    {"firstName": "Jane", "lastName": "Doe", "age": 45, "hairColor": "Black"},
+    {"firstName": "John", "lastName": "Smith", "age": 88, "hairColor": "Brown"},
+    {"firstName": "Millard", "lastName": "Fillmore", "age": 50, "hairColor": "Brown"},
+    {"firstName": "Maria", "lastName": "Smith", "age": 62, "hairColor": "Brown"}
   ]
   return (
     <div className="App">
--- react/props/src/components/PersonCard.js
+++ react/props/src/components/PersonCard.js
@@ -1,16 +1,30 @@
 import React, {Component} from 'react';
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            age: this.props.age
+        }
+    }
     render(){
-        const {firstName, lastName, age, hairColor} = this.props
+        const {firstName, lastName, hairColor} = this.props
         return(
             <div>
                 <h1>{firstName}, {lastName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={this.birthday}>Birthday 🎂</button>
             </div>
         )
     }
+
+    birthday = () => {
+        const {age} = this.state;
+        this.setState({age: age+1});
+    }
+
+
 }
 
 export default PersonCard
