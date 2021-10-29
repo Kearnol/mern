847843e154271ce2fc516e331f430f9b83ce9a2b
Devin Kearns <devin.kearns@outlook.com>
2021-10-29 13:48:55 -0400

    added functional component card

--- react/props/src/App.js
+++ react/props/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import PersonCard from './components/PersonCard';
+import FunkyCard from './components/FunkyPersonCard';
 
 
 
@@ -12,8 +13,11 @@ function App() {
     {"firstName": "Maria", "lastName": "Smith", "age": 62, "hairColor": "Brown"}
   ]
   return (
+    // <div className="App">
+    //   {person.map(person => {return <PersonCard firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor}/>})}
+    // </div>
     <div className="App">
-      {person.map(person => {return <PersonCard firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor}/>})}
+      {person.map(person => {return <FunkyCard firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor}/>})}
     </div>
   );
 }
