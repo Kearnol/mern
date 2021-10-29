bc26c85c5e8fbf7ee04b4242ba81e228253aa99d
Devin Kearns <devin.kearns@outlook.com>
2021-10-29 18:52:14 -0400

    form validations

--- react/props/src/components/Form.js
+++ react/props/src/components/Form.js
@@ -2,11 +2,17 @@ import React, {useState} from "react";
 
 const Form = () => {
 
-    const [firstname,setFirstname] = useState("")
-    const [lastname,setLastname] = useState("")
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
-    const [confpass, setConfpass] = useState("")
+    const [firstname,setFirstname] = useState("");
+    const [fnameerror, setFnameerror] = useState("");
+    const [lastname,setLastname] = useState("");
+    const [lnameerror, setLnameerror] = useState("");
+    const [email,setEmail] = useState("");
+    const [emailerror,setEmailerror] = useState("");
+    const [password,setPassword] = useState("");
+    const [passerror,setPasserror] = useState("");
+    const [confpass, setConfpass] = useState("");
+    const [conferror, setConferror] = useState("");
+    const [hasbeensubmitted, setHasbeensubmitted] = useState(false);
 
     const createUser = (e) => {
         e.preventDefault();
@@ -19,40 +25,136 @@ const Form = () => {
         setEmail("");
         setPassword("");
         setConfpass("");
+        setHasbeensubmitted(true)
+    }
+
+    const handleFname = (e) => {
+        e.preventDefault();
+        setFirstname(e.target.value)
+        if(e.target.value.length == 0){
+            setFnameerror("")
         }
+        else if(e.target.value.length < 2){
+            setFnameerror("Name must be at least 2 characters long");
+        }
+        else{
+            setFnameerror("")
+        }
+    }
+    
+    const handleLname = (e) => {
+        e.preventDefault();
+        setLastname(e.target.value)
+        if(e.target.value.length == 0){
+            setLnameerror("")
+        }
+        else if(e.target.value.length < 2){
+            setLnameerror("Name must be at least 2 characters long");
+        }
+        else{
+            setLnameerror("")
+        }
+    }
+
+    const handleEmail = (e) => {
+        e.preventDefault();
+        setEmail(e.target.value)
+        if(e.target.value.length == 0){
+            setEmailerror("")
+        }
+        else if(e.target.value.length < 5){
+            setEmailerror("Email must be at least 5 characters long");
+        }
+        else{
+            setEmailerror("")
+        }
+    }
+
+    const handlePasswords = (e) =>{
+        e.preventDefault();
+        setPassword(e.target.value);
+        if(e.target.value.length == 0){
+            setPasserror("");
+        }
+        else if (e.target.value.length < 8){
+            setPasserror("Password must be 8 characters");
+        }
+        else{
+            setPasserror("");
+        }
+    }
+
+    const confirmPass = (e) => {
+        e.preventDefault();
+        setConfpass(e.target.value);
+        if(e.target.value.length == 0){
+            setConferror("");
+        }
+        else if(e.target.value != password){
+            setConferror("Passwords don't match");
+        }
+        else{
+            setConferror("");
+        }
+    }
 
 
-    return(
-        <div>
-            <form onSubmit={ createUser } >
-                <div>
-                    <label for="firstname">First Name: </label>
-                    <input id="firstname" type ="text" onChange={(e)=>setFirstname(e.target.value)} value={firstname}/>
-                </div>
-                <div>
-                    <label for="lastname">Last Name: </label>
-                    <input id="lastname" type ="text" onChange={(e)=>setLastname(e.target.value)} value={lastname}/>
-                </div>
-                <div>
-                    <label for="email">Email: </label>
-                    <input id="email" type ="text" onChange={(e)=>setEmail(e.target.value)} value={email}/> 
-                </div>
-                <div>
-                    <label for="firstname">Password: </label>
-                    <input id="firstname" type ="password" onChange={(e)=>setPassword(e.target.value)} value={password}/> 
-                </div>
-                <div>
-                    <label for="confirm">Confirm Password: </label>
-                    <input id="confirm" type ="password" onChange={(e)=>setConfpass(e.target.value)} value ={confpass}/>
-                </div>
-                <input type="submit" value="Submit"/>
-            </form>
-            <h2>First Name: {firstname}</h2>
-            <h2>Last Name: {lastname}</h2>
-            <h2>Email: {email}</h2>
-            <h2>Password: *******</h2>
+    return (<div>
+            {
+            hasbeensubmitted?
+            <h1>Thanks for Submitting!</h1>:
+            <div>
+                <form onSubmit={ createUser }>
+                    <div>
+                        <label for="firstname">First Name: </label>
+                        <input id="firstname" type ="text" onChange={ handleFname } value={firstname}/>
+                        {
+                            fnameerror?
+                            <p style = {{color:'red'}}> {fnameerror}</p>:null
+                        }
+                    </div>
+                    <div>
+                        <label for="lastname">Last Name: </label>
+                        <input id="lastname" type ="text" onChange={ handleLname } value={lastname}/>
+                    </div>
+                        {
+                            emailerror?
+                            <p style = {{color:'red'}}> {lnameerror}</p>:null
+                        }
+                    <div>
+                        <label for="email">Email: </label>
+                        <input id="email" type ="text" onChange={ handleEmail } value={email}/> 
+                        {
+                            emailerror?
+                            <p style = {{color:'red'}}> {emailerror}</p>:null
+                        }
+                    </div>
+                    <div>
+                        <label for="firstname">Password: </label>
+                        <input id="firstname" type ="password" onChange={ handlePasswords } value={password}/>
+                        {
+                            passerror?
+                            <p style = {{color:'red'}}> {passerror}</p>:null 
+                        }
+                    </div>
+                    <div>
+                        <label for="confirm">Confirm Password: </label>
+                        <input id="confirm" type ="password" onChange={ confirmPass } value ={confpass}/>
+                        {
+                            conferror?
+                            <p style = {{color:'red'}}> {conferror}</p>:null 
+                        }
+                    </div>
+                    <input type="submit" value="Submit"/>
+                </form>
+                <h2>First Name: {firstname}</h2>
+                <h2>Last Name: {lastname}</h2>
+                <h2>Email: {email}</h2>
+                <h2>Password: *******</h2>
+            </div>
+            }
         </div>
     )
 }
 
-export default Form;
+export default Form
