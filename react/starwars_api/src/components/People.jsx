9db71b089e16415ad25df97c7f7a2fae0239dfe3
Devin Kearns <devin.kearns@outlook.com>
2021-11-04 21:55:41 -0400

    added homeland name & fixed homeland bug

--- react/starwars_api/src/components/People.jsx
+++ react/starwars_api/src/components/People.jsx
@@ -7,6 +7,7 @@ const People = () => {
     const {id} = useParams();
     const [resultData, setResultData] = useState("notSet");
     const history = useHistory();
+    const [homeworld, setHomeWorld] = useState("")
 
 
     useEffect(()=>{
@@ -19,23 +20,41 @@ const People = () => {
 
     function transfer(e){
         e.preventDefault();
+        console.log("homeworld", resultData.homeworld)
         let planetIDArr = resultData.homeworld.slice(-4);
-        let planetID = []
+        console.log("planetIDArr", planetIDArr)
+        var planetID = []
+        console.log(planetID)
         for (let x of planetIDArr){
+            console.log(x);
+            console.log("planetID", planetID)
             if (!isNaN(x)){
                 planetID.push(x);
-                planetID = planetID.join();
             }
         }
+        if (planetID.length > 1){
+            planetID = planetID.join("")
+        }else{planetID= planetID[0]}
         console.log(planetID)
         history.push(`/planets/${planetID}`)
     }
+
+    useEffect(()=> {
+        if(resultData != null){
+            axios.get(`${resultData.homeworld}`)
+            .then(res => {console.log("res", res); return res})
+            .then(res => {setHomeWorld(res.data.name)})
+            .catch(err => console.log(err))
+        }else{ return "n/a"
+        }}, [resultData])
+        
+        
 
     return(
         <div>
                 {
                 resultData == "notSet"? <img src="https://i.stack.imgur.com/X8C9v.gif"/> : resultData? (<>
-                <table>
+                <table className={"table"}>
                     <tr>
                         <th>Name</th>
                         <th>Eye Color</th>
@@ -51,10 +70,10 @@ const People = () => {
                         <td>{resultData.eye_color}</td>
                         <td>{resultData.gender}</td>
                         <td>{resultData.hair_color}</td>
-                        <td><button onClick={ transfer }>Homeworld</button></td>
+                        <td><button onClick={ transfer } className={"btn-primary btn-sm"} style={{backgroundColor:"#0d6efd"}}>{homeworld}</button></td>
                         <td>{resultData.mass}</td>
                         <td>{resultData.skin_color}</td>
-                        <td><a href={`https://www.google.com/search?q=google+${resultData.name}+star+wars`}>Learn More</a></td>
+                        <td><a href={`https://www.google.com/search?q=${resultData.name}+star+wars`}>Learn More</a></td>
                     </tr>
                 </table>
                 </>):
--- react/starwars_api/src/components/Search.jsx
+++ react/starwars_api/src/components/Search.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import { useHistory } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 const Search = () => {
@@ -13,21 +14,21 @@ const Search = () => {
         history.push(`/${resource}/${id}`) 
     }
 
-    return(
-        <>
-        <form onSubmit={ handlesubmit }>
-            <label> Search for:</label>
-            <select name="resource" id="resource" onChange={(e) => {setResource(e.target.value);console.log(resource)}}>
-                <option value="" selected disabled hidden>Select Resource</option>
-                <option value="people">People</option>
-                <option value="starships">Starships</option>
-                <option value="planets">Planets</option>
-
-            </select>
-            <input type="number" name="id" placeholder="id" onChange={(e)=> setID(e.target.value)}/>
-            <input type="submit" value="search"/>
-        </form>
-        </>
+    return(        
+            <form onSubmit={ handlesubmit } className="d-flex mt-2 align-items-center justify-content-sm-between">
+                <label> Search for: </label>
+                <select className="btn-sm btn-outline-warning dropdown-toggle mx-2" style={{color:"black"}} type="button" data-bs-toggle="dropdown" aria-expanded="false" name="resource" id="resource" onChange={(e) => {setResource(e.target.value);console.log(resource)}}>
+                    <option value="" selected disabled hidden>Select Resource</option>
+                    <option value="people">People</option>
+                    <option value="starships">Starships</option>
+                    <option value="planets">Planets</option>
+                </select>
+                <div class="input-group input-group-sm" style={{width:"auto"}}>
+                    <span class="input-group-text" id="inputGroup-sizing-sm">ID</span>
+                    <input type="number" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" style={{width:"60px", fontSize:"small"}} onChange={(e)=> setID(e.target.value)}/>
+                </div>
+                <input className={"btn btn-success btn-sm ms-2"} type="submit" value="search"/>
+            </form>
     )
 }
 
